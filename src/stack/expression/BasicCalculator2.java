b402f8ae06b8bb22ed3a4ac46b063e59ff4b4d4d
Khadija Ashraf <kashraf@Khadijas-MacBook-Pro.local>
2025-05-07 22:44:03 -0400

    Basic Calculator 2

--- src/stack/expression/BasicCalculator2.java
+++ src/stack/expression/BasicCalculator2.java
@@ -4,30 +4,61 @@ import java.util.Stack;
 
 public class BasicCalculator2 {
     public int calculate(String s) {
+    	// Intuition: in this approach as soon as we hit an operation '+,-,*,/'
+    	// we evaluate the expression value so far. but we evaluate 
     	
+    	// start with '+' sign because positive sign is imperative in the
+    	// start of every expression. even if an expression starts with a '-'
+    	// negative sign can incorporate a positive '+' sign too. 
+    	// for example +(-3) = -3.
+    	
+    	// another intuition is, to process an operation from left to right
+    	// we need two numbers. but when we hit an operator we only 
+    	// seen the first number. the second number is yet to come.
+    	
+    	// so, the trick is don't compute an operator until you reach to the 
+    	// next operator in the expression. only when we reach to an the next operator 
+    	// we see both the numbers involving that operator.
+    	// for example, 5 + 9 / 3; until we reach to '/' we don't get to see 5 , and 3.
+    	
+    	// the next trick is, to prioritize the '*, /' operators over '+,-'
+    	// we delay/defer the addition and subtraction by pushing those numbers
+    	// involving addition and subtraction. contrary to prioritize '*,/'
+    	// we calculate the expression so far as soon as we encounter
+    	// the previous sign among '*, or /'.
+    	
+    	// for an expression starting with a '-' negative sign we push '-num'
+    	// that is '-0' in the stack. because, num = 0 at the first iteration.
+   
+    	Stack<Integer> stack = new Stack<>();
     	int num = 0;
     	int res = 0;
-    	Stack<Integer> stack = new Stack<>();
-    	char sign = '+';
+    	char prevSign = '+'; 
     	
         for(int i = 0; i < s.length(); i++) {
-        	char c = s.charAt(i);
+        	char cur = s.charAt(i);
         	
-        	if (c == ' ') continue;
+        	if (cur == ' ') continue;
         	
-        	if(Character.isDigit(c)) {  // digit
-        		num = num * 10 + (c -'0');
+        	if(Character.isDigit(cur)) {  // digit
+        		num = num * 10 + (cur -'0');
         	} 
         	
-        	if (!Character.isDigit(c) || i == s.length() - 1){
-        		if(sign == '+') stack.push(num);
-        		else if (sign == '-') stack.push(-num);
-        		else if (sign == '*') stack.push(stack.pop() * num);
-        		else if (sign == '/') stack.push(stack.pop() / num);
+        	// process the non-digit, or the last num that does not have any sign after it.
+        	if (!Character.isDigit(cur) || i == s.length() - 1){ 
+        		// we are currently working on the previous sign,
+        		// Intuition: When we hit a sign we process the previous sign and 
+        		// update previous sign by the current sign.
+        		if(prevSign == '+') stack.push(num);
+        		else if (prevSign == '-') stack.push(-num);
+        		else if (prevSign == '*') stack.push(stack.pop() * num);
+        		else if (prevSign == '/') stack.push(stack.pop() / num);
         		
-        		sign = c;
-        		num = 0;
-        	}
+        		prevSign = cur; // set the current sign as previous sign
+        		// since we just processed an operator now set num to zero, 
+        		// cause we are going to see a new number after this operator
+        		num = 0; 
+    		}
         }
         for(int n: stack) {
         	res += n;
