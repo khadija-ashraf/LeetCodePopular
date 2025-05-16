5c6e0fbd9dc8dfbb9272947c8d9d0b536a15c10c
Khadija Ashraf <kashraf@Khadijas-MacBook-Pro.local>
2025-05-16 18:13:16 -0400

    1. Two Sum

--- src/hashtable/twoSum/TwoSum.java
+++ src/hashtable/twoSum/TwoSum.java
@@ -3,26 +3,42 @@ package hashtable.twoSum;
 import java.util.*;
 
 public class TwoSum {
+//	public int[] twoSum(int[] nums, int target) {
+//        if(nums.length <= 0) return new int[] {0,0};
+//        
+//        int[] res = new int[2];
+//        
+//        Map<Integer, Integer> map = new HashMap<>();
+//        
+//        for(int i = 0; i < nums.length; i++) {
+//        	
+//        	int pair = target - nums[i];
+//        	
+//        	if(map.containsKey(pair)) {
+//        		res[0] = map.get(pair);
+//        		res[1] = i;
+//        		return res;
+//        	}
+//        	map.putIfAbsent(nums[i], i);
+//        }
+//        return res;
+//	}
+	
 	public int[] twoSum(int[] nums, int target) {
-        if(nums.length <= 0) return new int[] {0,0};
-        
-        int[] res = new int[2];
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < nums.length; i++) {
-        	
-        	int pair = target - nums[i];
-        	
-        	if(map.containsKey(pair)) {
-        		res[0] = map.get(pair);
-        		res[1] = i;
-        		return res;
-        	}
-        	map.putIfAbsent(nums[i], i);
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Check every pair
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
         }
-        return res;
-	}
+
+        // No match found (problem says there is one, so this won't be reached)
+        return new int[] {};
+    }
 	public static void main(String[] args) {
 		TwoSum ob = new TwoSum();
 		
